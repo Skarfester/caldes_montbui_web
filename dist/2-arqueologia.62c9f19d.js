a34bde4624be0db37ad5ce7567e4291fc660e13b
Skarfester <18647291+Skarfester@users.noreply.github.com>
2024-11-13 13:08:51 -0600

    entrega

--- dist/2-arqueologia.62c9f19d.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("bloc_desp"),t=document.getElementById("llista_desp");e.addEventListener("click",()=>{t.classList.toggle("visible")});
-//# sourceMappingURL=2-arqueologia.62c9f19d.js.map
---
+++ dist/2-arqueologia.eedd24e9.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("bloc_desp"),t=document.getElementById("llista_desp");e.addEventListener("click",function(){t.classList.toggle("visible")});
+//# sourceMappingURL=2-arqueologia.eedd24e9.js.map
