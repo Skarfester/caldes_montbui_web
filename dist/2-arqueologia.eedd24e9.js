ab2cc5f4e10b1fffeda9589c26951accf98b8219
Skarfester <18647291+Skarfester@users.noreply.github.com>
2024-12-03 19:58:33 -0600

    suprimit dist

--- dist/2-arqueologia.eedd24e9.js
+++
@@ -1,2 +0,0 @@
-var e=document.getElementById("bloc_desp"),t=document.getElementById("llista_desp");e.addEventListener("click",function(){t.classList.toggle("visible")});
-//# sourceMappingURL=2-arqueologia.eedd24e9.js.map
