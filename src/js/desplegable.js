a83e1466159669e5248f93c0a5a4c04247bb8bb2
Skarfester <18647291+Skarfester@users.noreply.github.com>
2024-12-05 20:49:37 -0600

    animacions

--- src/js/desplegable.js
+++ src/js/desplegable.js
@@ -2,9 +2,8 @@
 
 // Variables DOM
 const bloc = document.getElementById("bloc_desp");
-const llista = document.getElementById("llista_desp");
 
 // Event listeners
 bloc.addEventListener("click", () => {
-	llista.classList.toggle("visible");
+	bloc.classList.toggle("visible");
 });
